22f945207d6ca91730ef1a1e2c05a63ab4381b90
Nehemias <yamithlopez1@gmail.com>
2025-02-28 18:11:33 -0500

    Creación de funciones

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let listaAmigos = [];
 let ulListaAmigos = document.getElementById('listaAmigos');
 let resultado = document.getElementById('resultado');
 
-function asignarTextoElemento(elemento, texto) {
+function asignarTextoElemento(elemento, texto) { 
     let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
@@ -23,24 +23,27 @@ function agregarAmigo() {
     listaAmigos.push(nombreAmigo);
     ulListaAmigos.innerHTML += `<li>${nombreAmigo}</li>`;
     limpiarBotonAñadir();
+    actualizarListaAmigos();
+
+
 }
 
-/*function agregarAmigo() {
-
- if(inputAmigo.value=='') {
-    alert('¡Debes ingresar un nombre!'); 
- }
- 
-    listaAmigos.push(inputAmigo.value); //push() añade un elemento al final de un array
-
-    console.log(listaAmigos);
-    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;    
-    console.log(listaAmigos);   
-    limpiarBotonAñadir();    
-    
-}	*/
-
 function sortearAmigo() {
+    if (listaAmigos.length < 2) {
+        alert('¡Debes agregar al menos dos amigos antes de sortear!');
+        return;
+    }
+    
+    // Sorteamos un amigo secreto
     let amigoSorteado = listaAmigos[Math.floor(Math.random() * listaAmigos.length)];
-    asignarTextoElemento('#resultado', amigoSorteado);
+    asignarTextoElemento('#resultado', `El amigo secreto es: ${amigoSorteado}`);
+}
+
+function actualizarListaAmigos() {
+    let ul = document.querySelector('#listaAmigos');
+    ul.innerHTML = '';
+
+    /*listaAmigos.forEach(amigo => {
+        ul.innerHTML += `<li>${amigo}</li>`;
+    });*/
 }
